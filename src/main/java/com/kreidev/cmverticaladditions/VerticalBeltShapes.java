b579b78150a8d01bb885e9e1a4a60adea0a77df7
Krei <criketgun@gmail.com>
2026-01-28 22:13:25 +0800

    add casing shapes

--- src/main/java/com/kreidev/cmverticaladditions/VerticalBeltShapes.java
+++ src/main/java/com/kreidev/cmverticaladditions/VerticalBeltShapes.java
@@ -25,9 +25,9 @@ public class VerticalBeltShapes {
             VERTICAL_END = VoxelShaper.forHorizontal(makeVerticalEnd(), Direction.SOUTH);
 
     private static final VoxelShaper
-            PARTIAL_CASING_MIDDLE = VoxelShaper.forHorizontal(box(0, 0, 5, 16, 11, 16), Direction.SOUTH),
-            PARTIAL_CASING_START = VoxelShaper.forHorizontal(box(0, 0, 5, 16, 11, 16), Direction.SOUTH),
-            PARTIAL_CASING_END = VoxelShaper.forHorizontal(box(0, 0, 5, 16, 11, 16), Direction.SOUTH);
+            PARTIAL_CASING_MIDDLE = VoxelShaper.forHorizontal(makeVerticalCasingMiddle(), Direction.SOUTH),
+            PARTIAL_CASING_START = VoxelShaper.forHorizontal(makeVerticalCasingStart(), Direction.SOUTH),
+            PARTIAL_CASING_END = VoxelShaper.forHorizontal(makeVerticalCasingEnd(), Direction.SOUTH);
 
     private static final Map<BlockState, VoxelShape> cache = new HashMap<>();
     private static final Map<BlockState, VoxelShape> collisionCache = new HashMap<>();
@@ -50,15 +50,14 @@ public class VerticalBeltShapes {
 
     private static VoxelShape getBeltShape(BlockState state) {
         Direction facing = state.getValue(BeltBlock.HORIZONTAL_FACING);
-        Direction.Axis axis = facing.getAxis();
         BeltPart part = state.getValue(BeltBlock.PART);
         BeltSlope slope = state.getValue(BeltBlock.SLOPE);
 
         if (slope == BeltSlope.VERTICAL) {
             return switch (part) {
-                case MIDDLE, PULLEY -> VERTICAL_MIDDLE.get(axis);
-                case START -> VERTICAL_START.get(axis);
-                case END -> VERTICAL_END.get(axis);
+                case MIDDLE, PULLEY -> VERTICAL_MIDDLE.get(facing);
+                case START -> VERTICAL_START.get(facing);
+                case END -> VERTICAL_END.get(facing);
             };
         }
 
@@ -74,11 +73,16 @@ public class VerticalBeltShapes {
         BeltPart part = state.getValue(BeltBlock.PART);
         BeltSlope slope = state.getValue(BeltBlock.SLOPE);
 
-        if (slope == BeltSlope.VERTICAL && part == BeltPart.MIDDLE)
-            return Shapes.empty();
+        if (slope == BeltSlope.VERTICAL) {
+            return switch (part) {
+                case MIDDLE, PULLEY -> PARTIAL_CASING_MIDDLE.get(facing);
+                case START -> PARTIAL_CASING_START.get(facing);
+                case END -> PARTIAL_CASING_END.get(facing);
+            };
+        }
 
-        //something went wrong
-        return Shapes.block();
+        //bad state
+        return Shapes.empty();
     }
 
     private static VoxelShape makeVerticalMiddle() {
@@ -99,5 +103,20 @@ public class VerticalBeltShapes {
                 box(1,3,4,15,13,15),
                 box(1,0,3,15,12,13)
         );
+    }
+
+    private static VoxelShape makeVerticalCasingMiddle() {
+        return box(0,0,5,16,16,16);
+    }
+
+    private static VoxelShape makeVerticalCasingStart() {
+        return Shapes.or(
+                box(0,0,5,16,16,16),
+                box(0, 0, 0, 16, 11, 16)
+        );
+    }
+
+    private static VoxelShape makeVerticalCasingEnd() {
+        return box(0,0,5,16,11,16);
     }
 }
